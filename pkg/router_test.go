8ddb786d8e127a2f1ed8097f0c4cbb1d418e7588
unknown <marcosstupnicki@gmail.com>
2025-03-08 11:48:12 -0800

    Add golog middleware

--- pkg/router_test.go
+++ pkg/router_test.go
@@ -2,13 +2,15 @@ package gowebapp
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestRouter_Method(t *testing.T) {
-	webapp := NewWebApp("test", "8080")
+	webapp, err := NewWebApp("test", "8080")
+	require.NoError(t, err)
 
 	ping := func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
@@ -18,6 +20,12 @@ func TestRouter_Method(t *testing.T) {
 	// Test that we can register routes directly
 	webapp.Get("/ping", ping)
 	webapp.Post("/ping", ping)
+
+	// Simple request to ensure ServeHTTP is wired
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+	rr := httptest.NewRecorder()
+	webapp.ServeHTTP(rr, req)
+	require.Equal(t, 200, rr.Code)
 
 	// Verify the router is working
 	require.NotNil(t, webapp.Router)
